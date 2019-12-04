37d068c842461c7582d7fbaec08f855e8a0ce4e8
jaywiles <jay.wiles@gmail.com>
2019-12-03 20:52:29 -0600

    five steps from bottom

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { Button, Container, Menu, Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core'
+import { MoreVert } from '@material-ui/icons'
 
 const Import = (props) => {
     // fill out this component
 
     return (
-        <p>Import Component</p>
+        <Container>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={props.fetchMakes}
+            >Import</Button>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Id</TableCell>
+                        <TableCell>Make</TableCell>
+                        <TableCell>Actions</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.makes.map((make) => (
+                        <TableRow key={make.MakeId}>
+                            <TableCell>{make["MakeId"]}</TableCell>
+                            <TableCell>{make["MakeName"]}</TableCell>
+                            <TableCell><MoreVert></MoreVert></TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </Container>
     )
 }
 
 export default Import
+
+// delete icon
+// morevert is extra credit
